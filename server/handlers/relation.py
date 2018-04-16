b08a4a6c27cd0f9dc21d92315fdc65dfc3fb3a44
无面 <wumian.mw@alibaba-inc.com>
2018-04-17 00:18:20 +0800

    approve relation

--- server/handlers/relation.py
+++ server/handlers/relation.py
@@ -24,12 +24,61 @@ class MakeRelationHandler(BaseHandler):
         logger.info(param)
         userId = self.get_current_user_id()
         userName = self.get_current_user_name()
-        relation = session.query(ApproveRelation).filter(ApproveRelation.applicant_id == userId).first()
+        relation = session.query(ApproveRelation).filter(and_(ApproveRelation.applicant_id == userId, ApproveRelation.approver_id.isnot(None))).first()
+        res = None
         try:
             if relation:
                 res = {'success': True}
                 self.write(res)
                 return
-            ticket = param['ticket']
-            relation = ApproveRelation(gmt_create = now, gmt_modified = now, applicant_id = userId, applicant_name = userName, approver_id = relation.approver_id, approver_name = relation.approver_name)
+            now = time.localtime()
+            relation = ApproveRelation(gmt_create = now, gmt_modified = now, applicant_id = userId, applicant_name = userName)
+            session.add(relation)
+            session.commit()
+            res = {'success': True}
+        except Exception as e:
+            session.rollback()
+            logger.exception(e)
+            res = {'success': False, 'errorMsg': str(e)}
+        self.write(res)
 
+class QueryRelationHandler(BaseHandler):
+    @authenticated
+    def get(self):
+        userId = self.get_current_user_id()
+        relation = session.query(ApproveRelation).filter(and_(ApproveRelation.applicant_id == userId, ApproveRelation.approver_id.isnot(None))).first()
+        res = None
+        if relation:
+            res = {'success': True}
+        else:
+            res = {'success': False}
+        self.write(res)
+
+class AcceptRelationHandler(BaseHandler):
+    @authenticated
+    def post(self):
+        param = self.request.body.decode('utf-8')
+        param = json.loads(param)
+        logger.info(param)
+        userId = self.get_current_user_id()
+        userName = self.get_current_user_name()
+        relation = session.query(ApproveRelation).filter(and_(ApproveRelation.applicant_id == param['applicantId'], ApproveRelation.approver_id.isnot(None))).first()
+        res = None
+        try:
+            if relation:
+                res = {'success': True}
+                self.write(res)
+                return
+            now = time.localtime()
+            relation.approver_id = userId
+            relation.approver_name = userName
+            relation.gmt_modified = now
+            session.flush()
+            session.commit()
+            res = {'success': True}
+        except Exception as e:
+            session.rollback()
+            logger.exception(e)
+            res = {'success': False, 'errorMsg': str(e)}
+        self.write(res)
+        
--- server/urls.py
+++ server/urls.py
@@ -3,11 +3,17 @@ from handlers.login import LoginHandler
 from handlers.create import CreateHandler
 from handlers.list import ListHandler
 from handlers.approve import ApproveHandler
+from handlers.relation import MakeRelationHandler
+from handlers.relation import QueryRelationHandler
+from handlers.relation import AcceptRelationHandler
 
 url_patterns = [
     (r"/", IndexHandler),
     (r"/login", LoginHandler),
     (r"/create", CreateHandler),
     (r"/list", ListHandler),
-    (r"/approve", ApproveHandler)
+    (r"/approve", ApproveHandler),
+    (r"/makerelation", MakeRelationHandler),
+    (r"/queryrelation", QueryRelationHandler),
+    (r"/acceptrelation", AcceptRelationHandler)
 ]
